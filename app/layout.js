37c2f9ce0d8fe1008a07bf3c5ec19f11a9fbfc31
joaozaguini <joaozaguini@users.noreply.github.com>
2026-07-18 23:47:19 -0300

    feat: replace native meta pixel with utmify pixel script

--- app/layout.js
+++ app/layout.js
@@ -10,18 +10,14 @@ export default function RootLayout({ children }) {
   return (
     <html lang="pt-BR">
       <head>
-        <Script id="meta-pixel" strategy="afterInteractive">
+        <Script id="utmify-pixel" strategy="afterInteractive">
           {`
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '1777482670090604');
-            fbq('track', 'PageView');
+            window.pixelId = "6a5c3a7d30b79c4b96078486";
+            var a = document.createElement("script");
+            a.setAttribute("async", "");
+            a.setAttribute("defer", "");
+            a.setAttribute("src", "https://cdn.utmify.com.br/scripts/pixel/pixel.js");
+            document.head.appendChild(a);
           `}
         </Script>
         <Script
@@ -32,12 +28,6 @@ export default function RootLayout({ children }) {
         />
       </head>
       <body>
-        <noscript>
-          <img height="1" width="1" style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=1777482670090604&ev=PageView&noscript=1"
-            alt=""
-          />
-        </noscript>
         {children}
       </body>
     </html>
